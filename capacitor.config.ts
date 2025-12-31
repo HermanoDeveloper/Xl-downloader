7e01301d96536b838b7d29d9ef0fae60616e65d6
HermanoDeveloper <xadrequeabel3@gmail.com>
2025-12-31 14:00:15 +0200

    feat: Configure project for Capacitor and TypeScript
    Set up project to use TypeScript for compilation and added Capacitor configuration to enable mobile app deployment. Updated `package.json` scripts and `tsconfig.json` for optimal build and module resolution.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.xadreklabs.statussaver',
+  appName: 'XL Status Saver',
+  webDir: 'dist'
+};
+
+export default config;
